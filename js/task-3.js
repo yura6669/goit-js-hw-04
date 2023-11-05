0c1c7682f4a1b2008a7ce1698f462191d0b8ae27
yura6669 <yurii.shyian.01@gmail.com>
2023-11-05 12:15:07 +0200

    Update task3

--- js/task-3.js
+++ js/task-3.js
@@ -12,7 +12,7 @@ const profile = {
     }
 };
 
-console.log("============ Task 2 ============");
+console.log("============ Task 3 ============");
 console.log(profile.getInfo());
 profile.changeUsername("Marco");
 console.log(profile.getInfo());
